bda468bbc0dee4ee50aa7bcde3ef5b760893aac2
Woody Goodricke <github@andrewgoodricke.com>
2015-06-30 16:57:10 -0300

    updated eslint and added serve.js to serve the static site

--- contentful_to_files.js
+++ contentful_to_files.js
@@ -1,20 +1,21 @@
-var contentful = require("contentful");
-var config     = require("./config.json").contentful;
-var fs         = require("fs");
-var path       = require("path");
+var contentful = require('contentful');
+var config     = require('./config.json').contentful;
+var fs         = require('fs');
+var path       = require('path');
 // var util       = require("util");
 
 var client     = contentful.createClient({
 	// A valid access token within the Space
-	accessToken : config.accessToken
-  , space       : config.space.id
-  , secure      : true
+	accessToken: config.accessToken
+  , space      : config.space.id
+  , secure     : true
 
 	// host: 'cdn.contentful.com'
 });
 
 
 var stringed = function (data) {
+	'use strict';
 	// Convert circular references to [Circular]. See:
 	// http://stackoverflow.com/questions/30236820/typeerror-converting-circular-structure-to-json-find-error-in-json
 	// return util.inspect(data, {"depth" : null});
@@ -23,16 +24,16 @@ var stringed = function (data) {
 
 
 var object_to_json_file = function (data, filename) {
-	"use strict";
+	'use strict';
 
-	var file_location = path.resolve(__dirname, config.space.location_to_store + filename + ".json");
+	var file_location = path.resolve(__dirname, config.space.location_to_store + filename + '.json');
 	data = stringed(data);
 
 	fs.writeFile(file_location, data, function (err) {
 		if (err) {
 			throw err;
 		}
-		console.log("Saved: " + file_location);
+		console.log('Saved: ' + file_location);
 	});
 };
 
@@ -44,11 +45,12 @@ var object_to_json_file = function (data, filename) {
 
 
 Object.keys(config.space.content_types).forEach(function(content_name) {
+	'use strict';
 	var content_type = config.space.content_types[content_name];
 
 	// console.log("content_type", content_type, "content_name", content_name);
 	// Get Assets using callback interface
-	client.entries({"content_type" : content_type}, function(err, entries) {
+	client.entries({'content_type': content_type}, function(err, entries) {
 		if (err) { console.log(err); return; }
 
 		object_to_json_file(entries, content_name);
--- tmp_get_json_file.js
+++
@@ -1,6 +0,0 @@
-// Get file
-var file    = require("./store/authors.json");
-var log_obj = require("./modules/log_object");
-
-
-log_obj("Object from file", file);
